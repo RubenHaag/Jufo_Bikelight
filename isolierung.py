27c2be4f919c6da1f5c4603b9cd1432ad446289c
grand-farid-2025 <farid.tabrizi@aol.com>
2016-07-10 18:07:41 +0200

    Isolierung

---
+++ isolierung.py
@@ -0,0 +1,15 @@
+class Isolierung:
+    def __init__(self, außenTemperatur, innenTemperatur, isolierungProzent):
+        self.außenTemperatur = außenTemperatur
+        self.innenTemperatur = innenTemperatur
+        self.isolierungProzent = isolierungProzent
+
+    def außenTemperatur_druck(self):
+        print(self.marke)
+        
+    def innenTemperatur_druck(self):
+        print(self.farbe)
+
+    def isolierungProzent_druck(self):
+        print(self.geschwindigkeit)
+
