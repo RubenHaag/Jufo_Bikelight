d115fc5f5de66bedae4c30bcd73ee7f865021365
grand-farid-2025 <farid.tabrizi@aol.com>
2016-07-10 18:23:26 +0200

    Verbrauch

---
+++ Verbrauch einer Geräteklasse.py
@@ -0,0 +1,9 @@
+class Verbrauch_einer_Geräteklasse:
+    def __init__(self, geräteanzahl, verbrauch, gesamtverbrauch, zeit):
+        self.geräteanzahl = geräteanzahl
+        self.verbrach = verbrauch
+        self.gesamtverbrauch = gesamtverbrauch
+        self.zeit = zeit
+
+    def alles():
+        self.gesamtverbrauch = self.geräteanzahl * self.verbrauch * self.zeit
