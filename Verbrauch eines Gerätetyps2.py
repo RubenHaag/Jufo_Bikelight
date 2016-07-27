f23266475a16d4dd443beae133165d06cbde2871
Haydn-Wien <jakob@degmair-berlin.de>
2016-07-27 15:31:51 +0200

    Verbrauch eines Gerätetyps (korrigiert)

---
+++ Verbrauch eines Gerätetyps2.py
@@ -0,0 +1,16 @@
+class Verbrauch_einer_Geräteklasse:
+    def __init__(self, geräteanzahl, verbrauch, zeit):
+        self.geräteanzahl = geräteanzahl
+        self.verbrauch = verbrauch
+        self.zeit = zeit
+
+    def alles(self):
+        gesamtverbrauch = self.geräteanzahl * self.verbrauch * self.zeit
+        if 0 <= gesamtverbrauch < 1000:
+            print("Der Gesamtverbrauch beträgt",gesamtverbrauch,"Wh.")
+        elif 1000 <= gesamtverbrauch:
+            kilogesamtverbrauch = gesamtverbrauch / 1000
+            print("Der Gesamtverbrauch beträgt",kilogesamtverbrauch,"kWh.")
+        else:
+            print("Falscher Wert!")
+
