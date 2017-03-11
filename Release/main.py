c12ba2e15586e4f8b0ffffb5d6c29b9a242d0eca
RubenHaag <rubenhaag1@gmail.com>
2017-03-11 20:33:46 +0000

    Bildwechsel funktioniert endlich

--- Release/main.py
+++ Release/main.py
@@ -4,15 +4,16 @@ from time import time, sleep
 from math import pi, sin, cos
 from PIL.Image import open
 from neopixel import Adafruit_NeoPixel, Color
+from sys import exit
+from os.path import isfile
 import RPi.GPIO as gp
 
 
-path = "/home/pi/Jufo_Bikelight/Release/Bilder/Herz"
-
-breite, höhe = im.size	#die Breite und die höhe des Bildes wird ausgelesen
-breite = breite /2 	#damit der Koordinatenmittelpunkt in die mitte des Bildes Kommt
-höhe = höhe / 2 
-
+path = "/home/pi/Bilder/Beispiel.png"
+im = open(path)
+pix = im.load()
+breite = 0
+höhe = 0
 
 # LED-Streifen-Konfiguration:
 LED_COUNT       = 140      	# LED-Anzahl.
@@ -32,7 +33,7 @@ w = 0       # Winkelgeschwindigkeit
 # erschaffen einer Liste ür die R Radien
 radien = [0 for x in range(0, int(LED_COUNT/ANZAHL_STREIFEN))]
 for i in range(0, int(LED_COUNT/ANZAHL_STREIFEN)): 	# Erschaffen der Radien
-    radien[i] = int(i+1+minR)
+	radien[i] = int(i+1+minR)
 
 # Startanzeige
 
@@ -65,28 +66,51 @@ def startPrint():
 	line(50)
 	print("xx km/h")
 
-def bildNeuLaden():
-	global pix
-	# Ein Bild kann nun als png oder als jpg gegeben sein
-	try:
-		im = open(path + "png")
-	except:
-		im = open(path + "jpeg")
-	else:
-		im = open(path + "jpg")
-	pix = im.load()
 
 def bildAuslesen(winkel, rad):
-
+	global pix
 	x = int(round(cos(winkel) * -rad + breite))   # Berechnung der X-Koordinate
 	y = int(round(sin(winkel) * rad + höhe))   	# Berechnung der Y Koordinate
 
-    try:
+	try:
 		r,g,b = pix[x, y]		# auslesen eines Pixels
 	except:
 		r,g,b,_ = pix[x, y]
+	return Color(g, r, b)
 
 
+def neuesBild(path):
+	global pix
+	global breite
+	global höhe
+	if isfile(path):
+		im = open(path)
+		pix = im.load()
+		breite, höhe = im.size	#die Breite und die höhe des Bildes wird ausgelesen
+		breite = breite /2 	#damit der Koordinatenmittelpunkt in die mitte des Bildes Kommt
+		höhe = höhe / 2
+	elif isfile(path+ "png"):
+		im = open(path + "png")
+		pix = im.load()
+		breite, höhe = im.size	#die Breite und die höhe des Bildes wird ausgelesen
+		breite = breite /2 	#damit der Koordinatenmittelpunkt in die mitte des Bildes Kommt
+		höhe = höhe / 2 
+	elif isfile(path+"jpg"):
+		im = open(path + "jpg")
+		pix = im.load()
+		breite, höhe = im.size	#die Breite und die höhe des Bildes wird ausgelesen
+		breite = breite /2 	#damit der Koordinatenmittelpunkt in die mitte des Bildes Kommt
+		höhe = höhe / 2
+	elif isfile(path+"jpeg"):
+		im = open(path + "jpeg")
+		pix = im.load()
+		breite, höhe = im.size	#die Breite und die höhe des Bildes wird ausgelesen
+		breite = breite /2 	#damit der Koordinatenmittelpunkt in die mitte des Bildes Kommt
+		höhe = höhe / 2 	
+	else:
+		print("Dateityp des Bildes wird nicht unterstützt...")
+		sleep(0.01)
+	
 
 
 
@@ -114,50 +138,26 @@ def streifenBedienen(t, w):
 
 	for i in range(u, M):
 
-		streifen.setPixelColor(i, bildAuslesen(beta, radien[i-u-1]))
+		streifen.setPixelColor(i, bildAuslesen(beta, radien[i-u]))
 
-		streifen.setPixelColor(i + M, bildAuslesen(delta, radien [i-u-1]))
+		streifen.setPixelColor(i + M, bildAuslesen(delta, radien [i-u]))
 
-def streifenBedienen():
-    global radien
-    global T
-    global t
-    global streifen
-    global w
-
-    alpha = w * t 				#Ausrechnen des Winkels in Bogenmaß
-    beta  = alpha + pi / 2
-    gamma = beta  + pi / 2
-    delta = gamma + pi / 2
-
-    n = streifen.numPixels()
-    u = int(n/ANZAHL_STREIFEN)
-    M = int(n/2)
-
-    for i in range(u):
-        u-= 1		
-        streifen.setPixelColor(i, bildAuslesen(alpha, radien[u]))              
-        streifen.setPixelColor(i, bildAuslesen(gamma, radien[u])) 
-		
-    u = int(n/ANZAHL_STREIFEN)
-
-    for i in range(u, M):
-        streifen.setPixelColor(i, bildAuslesen(beta, radien[i-u]))
-        streifen.setPixelColor(i + M, bildAuslesen(delta, radien[i-u]))
 
 def main():
 	global radien
 	global streifen
+	global pix
+	global T
 	n = 0			#Zählervariable
-
+	neuesBild(path)
 	gp.setmode(gp.BCM)          # Welche Nummern für die Pins verwendet werden
 	gp.setwarnings(False)       # Keine Warnungen
 	gp.setup(MAGNET_PIN, gp.IN) # Anschluss
 
 	
-    #Erschaffen des Led-Streifen-Objekts
-    streifen = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, 
-                                 LED_INVERT, LED_BRIGHTNESS)
+	#Erschaffen des Led-Streifen-Objekts
+	streifen = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, 
+								 LED_INVERT, LED_BRIGHTNESS)
 
 
 	streifen.begin()    #initialisieren des LED-Streifens
@@ -165,12 +165,12 @@ def main():
 
 
 
-    while True:                             
-        t1 = time()		#Startzeit der Umdrehung t1
+	while True:                             
+		t1 = time()		#Startzeit der Umdrehung t1
 		
-        if gp.input(MAGNET_PIN) == False:	#Zeitmessung einmal je Umdrehung
+		if gp.input(MAGNET_PIN) == False:	#Zeitmessung einmal je Umdrehung
 			
-            w = 2 * pi / T 	#Berrechnen der Aktuellen Winkelgeschwindigkeit
+			w = 2 * pi / T 	#Berrechnen der Aktuellen Winkelgeschwindigkeit
 			
 			while gp.input(MAGNET_PIN) == False:
 				t = time() - t1 			#Ausrechnen der größe des Zeitabschnitts
@@ -182,11 +182,10 @@ def main():
 					for i in range(0, LED_COUNT):
 						streifen.setPixelColor(i, Color(0,0,0))
 						streifen.show()
-			n++
+			n +=1
 			if n > 3:				#jede dritte Umdrehung wird das bild neu in den Zwischenspeicher gepackt
 				n = 0
-				im = open(path)
-				pix = im.load()
+				neuesBild("/home/pi/Bilder/Bild.")
 
 			print("\b\b\b\b\b\b\b" + str(int(w * 0.3*3.6)) + " km/h")
 			T = time() - t1              #Ausrechnen von T nach T = t2 - t1
@@ -194,4 +193,4 @@ def main():
 
 	
 if __name__ == '__main__':
-    main()
+	main()
